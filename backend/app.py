ea776630fe3c30cf9cdfb3e89e3d0c43e81fec13
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-06-04 17:49:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -136,5 +136,7 @@ def convert_playlist():
     return Response(stream_with_context(generate()), mimetype="text/event-stream")
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8080)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
