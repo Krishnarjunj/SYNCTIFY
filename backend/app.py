3ec6d336deb7d752047c98f5d09380a5710a221d
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-06-04 17:00:20 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -143,5 +143,6 @@ def convert_playlist():
         print(traceback.format_exc())
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8080)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
